314124fba7159c6c5d7904de7337249117905564
Ubuntu <ubuntu@ip-172-31-93-23.ec2.internal>
2019-10-31 00:22:32 +0000

    Menu

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -7,8 +7,22 @@
 module Handler.Home where
 
 import Import
+import Text.Lucius
+import Text.Julius
 --import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
+
+getPage1R :: Handler Html
+getPage1R = do
+    defaultLayout $ do
+        toWidgetHead $(juliusFile "templates/page1.julius")
+        toWidgetHead $(luciusFile "templates/page1.lucius")
+        $(whamletFile "templates/page1.hamlet")
+
+getPage2R :: Handler Html
+getPage2R = do
+    defaultLayout $ do
+        $(whamletFile "templates/page2.hamlet")
 
 getHomeR :: Handler Html
 getHomeR = do
@@ -21,15 +35,32 @@ getHomeR = do
             }
         |]
         toWidgetHead [lucius|
+            body {
+                background-image: url(@{StaticR rpg_jpg});
+                background-size: cover;
+                background-repeat: no-repeat;
+            }
             h1 {
-                color: red;
+                color: cyan;
+            }
+            ul {
+                list-style: none;
+            }
+            li {
+                display: inline;
             }
         |]
         [whamlet|
             <h1>
                 SITE EM CONSTRUCAO!
                 
-            <img src=@{StaticR rpg_jpg}>
+            <ul>
+                <li>
+                    <a href=@{Page1R}
+                        Pagina1
+                <li>
+                    <a href=@{Page2R}
+                        Pagina2
                 
             <button class="btn btn-danger" onclick="ola()">
                 OK

74a9f654a9c39786bc829f2e05e521592ad91caa
Ubuntu <ubuntu@ip-172-31-93-23.ec2.internal>
2019-12-04 01:33:52 +0000

    Cadastrar Talento

---
+++ src/Handler/Talento.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Talento where
+
+import Import
+import Text.Lucius
+import Text.Julius
+--import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+
+formTalento :: Form Talento
+formTalento = renderBootstrap $ Talento 
+    <$> areq textField "Nome: " Nothing
+    <*> areq textField "Pré-requisito: " Nothing
+    <*> areq textField "Descrição: " Nothing
+
+getTalentoR :: Handler Html
+getTalentoR = do
+    (widget,enctype) <- generateFormPost formTalento
+    
+    defaultLayout $ do
+        toWidgetHead $(luciusFile "templates/main.lucius")
+        toWidgetHead $(luciusFile "templates/talento.lucius")
+        $(whamletFile "templates/talento.hamlet")
+        
+postTalentoR :: Handler Html
+postTalentoR = do
+    ((result,_),_) <- runFormPost formTalento
+    case result of 
+        FormSuccess talento -> do
+            runDB $ insert talento
+            redirect TalentoR
+        _ -> redirect HomeR 

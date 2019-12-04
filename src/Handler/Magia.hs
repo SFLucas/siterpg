9c45eaf6e9b1f94aa026999a984fdc605af97e09
Ubuntu <ubuntu@ip-172-31-93-23.ec2.internal>
2019-12-04 12:34:37 +0000

    Cadastrar Magia

---
+++ src/Handler/Magia.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Magia where
+
+import Import
+import Text.Lucius
+import Text.Julius
+--import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+
+formMagia :: Form Magia 
+formMagia = renderBootstrap $ Magia 
+    <$> areq textField "Nome: " Nothing
+    <*> areq intField "Nivel: " Nothing
+    <*> areq textField "Tipo: " Nothing
+    <*> areq textField "Custo: " Nothing
+    <*> areq textField "Descrição: " Nothing
+    <*> areq intField "Alcance: " Nothing
+
+getMagiaR :: Handler Html
+getMagiaR = do
+    (widget,enctype) <- generateFormPost formMagia
+    
+    defaultLayout $ do
+        toWidgetHead $(luciusFile "templates/main.lucius")
+        toWidgetHead $(luciusFile "templates/magia.lucius")
+        $(whamletFile "templates/magia.hamlet")
+        
+postMagiaR :: Handler Html
+postMagiaR = do
+    ((result,_),_) <- runFormPost formMagia
+    case result of 
+        FormSuccess magia -> do
+            runDB $ insert magia
+            redirect MagiaR
+        _ -> redirect HomeR
